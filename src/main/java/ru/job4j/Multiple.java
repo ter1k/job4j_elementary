f025cc47e42f53787dce0d015c0c8ce8072bd1f9
ter1k <terletskynick@gmail.com>
2022-06-08 19:53:03 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public class Multiple {
+
+    public static void main(String[] args) {
+
+        for (int i = 2; i < 10; i++) {
+
+            System.out.println("1 * " + i + " = " + i);
+
+        }
+
+    }
+
+}
---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Nickolai");
+        System.out.println("16.08.2070");
+
+    }
+
+}
