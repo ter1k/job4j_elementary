60bfcaac6a23e185cf4f6ba386182467afd976f8
ter1k <terletskynick@gmail.com>
2022-06-08 20:54:27 +0300

    0. Что такое переменная. [#3610]

--- src/main/java/ru/job4j/calculate/Variables.java
+++ src/main/java/ru/job4j/calculate/Variables.java
@@ -5,6 +5,5 @@ public class Variables {
         int age = 18;
         float length = 78.0332F;
         boolean baby = true;
-        System.out.println(age + " " + length + " " + baby);
     }
 }
