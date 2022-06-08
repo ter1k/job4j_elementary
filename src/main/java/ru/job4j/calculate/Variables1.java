1bd025fbdd334e74d32962ee574198d16cd91ff6
ter1k <terletskynick@gmail.com>
2022-06-08 21:52:56 +0300

    2. Переменные. [#172635 #310522]

---
+++ src/main/java/ru/job4j/calculate/Variables1.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculate;
+
+public class Variables1 {
+    public static void main(String[] args) {
+        short age = 130;
+        char sex = 'm'; //m - male, f - female
+        float height = 185.67F;
+        short brickamt = 580; //amt = amount
+        int bacteriesamt = 120000000; //тонкая грань с long, но не превышает её в контексте йогуртов
+        long spacedist = 250000000000000L; //dist = distance
+        boolean entrypermit = true; //permit = permission
+        long filesize = 499299221504L;
+    }
+}
