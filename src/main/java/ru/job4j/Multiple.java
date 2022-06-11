1b80dd0fc1e2cc0f66491437450ef30037acb7d8
ter1k <terletskynick@gmail.com>
2022-06-11 14:28:18 +0300

    2. Переменные. [#172635 #310522]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -3,13 +3,9 @@ package ru.job4j;
 public class Multiple {
 
     public static void main(String[] args) {
-
         for (int i = 2; i < 10; i++) {
-
             System.out.println("1 * " + i + " = " + i);
-
         }
-
     }
 
 }
