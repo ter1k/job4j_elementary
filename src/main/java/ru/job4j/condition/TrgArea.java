5b0aecf75b008f9657850aa77bb572318267ba2e
ter1k <terletskynick@gmail.com>
2022-06-13 12:17:56 +0300

    slight code changes

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -3,8 +3,7 @@ package ru.job4j.condition;
 public class TrgArea {
     public static double area(double a, double b, double c) {
         double p = (a + b + c) / 2;
-        double rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-        return rsl;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     public static void main(String[] args) {
